dd7e7918e69c93d96785673f707c10e53bf9435e
shams <shamsazeem20@gmail.com>
2020-04-20 11:44:14 +0500

    Make event loop time too large

--- src/main/java/konduit/demo/metrics/App.java
+++ src/main/java/konduit/demo/metrics/App.java
@@ -9,12 +9,21 @@ import ai.konduit.serving.model.ModelConfigType;
 import ai.konduit.serving.pipeline.step.ImageLoadingStep;
 import ai.konduit.serving.pipeline.step.ModelStep;
 import com.jayway.restassured.response.Response;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.micrometer.MicrometerMetricsOptions;
+import io.vertx.micrometer.VertxPrometheusOptions;
+import io.vertx.micrometer.backends.BackendRegistries;
 import org.nd4j.linalg.io.ClassPathResource;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import static ai.konduit.serving.metrics.MetricType.*;
 import static com.jayway.restassured.RestAssured.given;
@@ -26,6 +35,7 @@ import static com.jayway.restassured.RestAssured.given;
 public class App 
 {
     public static void main( String[] args ) throws IOException {
+
         InferenceConfiguration inferenceConfiguration = InferenceConfiguration.builder()
                 .servingConfig(ServingConfig.builder()
                         .httpPort(9008)
@@ -54,7 +64,17 @@ public class App
                         ).build())
                 .build();
 
-        DeployKonduitServing.deployInference(inferenceConfiguration,
+        MicrometerMetricsOptions micrometerMetricsOptions = new MicrometerMetricsOptions()
+                .setMicrometerRegistry(new PrometheusMeterRegistry(PrometheusConfig.DEFAULT))
+                .setPrometheusOptions(new VertxPrometheusOptions().setEnabled(true));
+        BackendRegistries.setupBackend(micrometerMetricsOptions);
+
+        VertxOptions vertxOptions = new VertxOptions().setMetricsOptions(micrometerMetricsOptions)
+                .setMetricsOptions(micrometerMetricsOptions).setMaxEventLoopExecuteTime(1000000L).setMaxEventLoopExecuteTimeUnit(TimeUnit.SECONDS);
+
+        DeploymentOptions deploymentOptions = new DeploymentOptions().setConfig(new JsonObject(inferenceConfiguration.toJson()));
+
+        DeployKonduitServing.deployInference(vertxOptions, deploymentOptions,
                 handler -> {
                     if(handler.succeeded()) {
                         Random random = new Random(System.currentTimeMillis());
